4389a1aeef7f68e9c3f3f4dc3ee20457276f2868
Keivin <keivinbr13@gmail.com>
2020-09-04 12:21:33 -0500

    build rename

---
+++ docs/precache-manifest.f7e7e929a904802f28f22a66dcb0ef1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9af8c0f4fe43765145b9247c7ff004c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6719ecf04c3233b6ba4b",
+    "url": "/static/css/main.7b8e7efa.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6719ecf04c3233b6ba4b",
+    "url": "/static/js/main.4e098485.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
